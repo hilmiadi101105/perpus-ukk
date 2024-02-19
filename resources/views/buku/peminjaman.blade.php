06babef91f3ed13461c2a3d649b02b4b04b7172a
Hilmi Adi <fauzanhilmiadi@gmail.com>
2024-02-19 15:02:11 +0700

    edit peminjaman

--- resources/views/buku/peminjaman.blade.php
+++ resources/views/buku/peminjaman.blade.php
@@ -6,7 +6,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <div class="mb-4">
-                        <a href="{{ route('peminjaman.tambah') }}" class="bg-blue-500 hover:bg-blue-700 text-black border font-bold py-2 px-4 rounded">
+                        <a href="{{ route('peminjaman.tambah') }}" class="bg-blue-500 hover:bg-blue-700 text-white border font-bold py-2 px-4 rounded btn btn-primary  ">
                             + Tambah Data Peminjaman 
                         </a>
                     </div>
@@ -34,7 +34,7 @@
                                         @if($p->status === 'Dipinjam')
                                             <form action="{{ route('peminjaman.kembalikan', $p->id) }}" method="post">
                                                 @csrf
-                                                <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-black border font-bold py-2 px-4 rounded">
+                                                <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white border font-bold py-2 px-4 rounded btn btn-success">
                                                     Kembalikan
                                                 </button>
                                             </form>
